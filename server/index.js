656ab650862c0ffc1f6d4c419db1787ec9eeadab
confidenceaffang <coaffa01@wsc.edu>
2025-03-17 16:08:02 -0500

    implemeted serverside, database and routes

--- server/index.js
+++ server/index.js
@@ -2,16 +2,14 @@ const express = require("express")
 const dotenv = require("dotenv")
 dotenv.config()
 const cors = require("cors")
-
+const gameRoutes = require("./gameRoutes")
 const app = express()
 
-app.use(cors({origin:"http://localhost:5173"}))
+app.use(cors())
 
-app.use(express.json)
+app.use(express.json())
 
-app.get("/api/test", (request, response) =>{
-    response.json({message: "Backend is started"})
-})
+app.get("/api", gameRoutes)
 const port = process.env.port || 3000
 
 app.listen(port, () =>{console.log(`Server running on port ${port}`)})
