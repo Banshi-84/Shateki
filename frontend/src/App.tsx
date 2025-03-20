ca5d1e67cee0b0c13b7aa41315e4df0118a88349
HaruhikoYoshida <harry5648564@gmail.com>
2025-03-20 12:46:39 -0500

    Added more info

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,29 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Game } from "./game/Game";
 import "./App.css";
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [game, setGame] = useState<Game | null>(null);
 
   useEffect(() => {
     if (canvasRef.current) {
-      new Game(canvasRef.current);
+      const newGame = new Game(canvasRef.current);
+      setGame(newGame);
+
+      const gameLoop = () => {
+        newGame.update();
+        requestAnimationFrame(gameLoop);
+      };
+
+      gameLoop();
     }
   }, []);
 
   return (
     <div>
       <h1>Shateki Game</h1>
-      <canvas ref={canvasRef} width={800} height={600} style={{ border: "1px solid black" }} />
+      <canvas ref={canvasRef} width={800} height={600} />
     </div>
   );
 }
