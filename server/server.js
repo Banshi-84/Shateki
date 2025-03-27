8342038d1cff49ba207c8d3700efd1b824051d2d
Confidence Mawuli Affang <coaffa01@wsc.edu>
2025-03-27 13:45:25 -0500

    changed cors

--- server/server.js
+++ server/server.js
@@ -9,10 +9,11 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json());
-app.use(cors({origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']}));
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'https://shateki-front.vercel.app',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
